14d8baaf99466a5e6954c59d326141d5cfc3ab58
Mateusz Chocianowski <ch.mateusz@hotmail.com>
2022-05-10 16:05:54 +0100

    QC-30 QC-58 QC-24 #comment Create a navbar

---
+++ src/components/NavbarMain.js
@@ -0,0 +1,42 @@
+import { Navbar, Nav, NavDropdown,Form,FormControl, Button, Container } from "react-bootstrap";
+
+const NavbarMain=()=>{
+
+    return (
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">Fantastic 4</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form className="d-flex">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+    );
+}
+export default NavbarMain
