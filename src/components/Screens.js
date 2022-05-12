3556f0c299b5df2aa9715c6c6536eff27e7e58ff
King.Dev <King.Dev>
2022-05-12 10:16:44 +0100

    Added Screen Page & imported images

---
+++ src/components/Screens.js
@@ -0,0 +1,19 @@
+import Screen1 from "../Images/Screen 1.png";
+import Screen2 from "../Images/Screen 2.png";
+import DirectorBox from "../Images/Director Box.png";
+
+
+const Screens = () => {
+    return (
+
+
+
+
+
+
+
+
+
+    );
+};
+export default Screens;
