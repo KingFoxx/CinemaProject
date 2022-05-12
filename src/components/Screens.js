1fe05258cec5c38d50b6b633efc9de57e04e984c
King.Dev <King.Dev>
2022-05-12 11:07:41 +0100

    Added prices, need to add concession

--- src/components/Screens.js
+++ src/components/Screens.js
@@ -1,18 +1,34 @@
 import Screen1 from "../Images/Screen 1.png";
 import Screen2 from "../Images/Screen 2.png";
 import DirectorBox from "../Images/Director Box.png";
+import "..//App.css";
 
 
 const Screens = () => {
     return (
-
-
-
-
-
-
-
-
+<div class="row">
+  <div class="column">
+    <img src={Screen1} alt="Snow" style="width:100%"/>
+    <p>
+        Adult :
+        Child :
+    </p>
+  </div>
+  <div class="column">
+    <img src={Screen2} alt="Forest" style="width:100%"/>
+    <p>
+        Adult :
+        Child :
+    </p>
+  </div>
+  <div class="column">
+    <img src={DirectorBox} alt="Mountains" style="width:100%"/>
+    <p>
+        Adult :
+        Child :
+    </p>
+  </div>
+</div>
 
     );
 };
