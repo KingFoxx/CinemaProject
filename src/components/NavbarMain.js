cdf5fe9c9c9b1d583c4fb29ce948d41795b7f2be
King.Dev <King.Dev>
2022-05-12 14:01:43 +0100

    Added Coming soon & on now pages, connected to main nav bar

--- src/components/NavbarMain.js
+++ src/components/NavbarMain.js
@@ -13,11 +13,11 @@ const NavbarMain=()=>{
               <Nav className="me-auto">
                 <Nav.Link as={Link} to={"/"}>Home</Nav.Link>
                 <NavDropdown title="Galleries" id="basic-nav-dropdown">
-                  <NavDropdown.Item as={Link} to={"/"}>What's On?</NavDropdown.Item>
-                  <NavDropdown.Item as={Link} to={"/"}>Coming Soon...</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to={"/OnNow"}>What's On?</NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to={"/Coming"}>Coming Soon...</NavDropdown.Item>
                 </NavDropdown>
                 <Nav.Link as={Link} to={"/Classification"}>Classications</Nav.Link>
-                <Nav.Link as={Link} to={"/"}>Welcome to the BIG screen</Nav.Link>
+                <Nav.Link as={Link} to={"/Screens"}>Welcome to the BIG screen</Nav.Link>
                 <NavDropdown title="Locations" id="basic-nav-dropdown">
                   <NavDropdown.Item as={Link} to={"/gettingthere"}>Finding Us?</NavDropdown.Item>
                   <NavDropdown.Item as={Link} to={"/whatsnear"}>What's Near Us?</NavDropdown.Item>
