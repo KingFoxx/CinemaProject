e2151a452ddd5e6e5b0d573a5e04bc7c2e91a896
Salman Nabi <salmannabi183@yahoo.com>
2022-05-13 13:31:20 +0100

    QC-75 #comment working on back-end implementation for discussion form submission

--- src/components/Discussion.js
+++ src/components/Discussion.js
@@ -1,52 +1,68 @@
 import { render } from "@testing-library/react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useReducer, useState } from 'react';
+
+const formReducer = (state, event) => {
+  return {...state, [event.name]: event.value}
+}
 
 const Discussion=()=> {
 
-    const handleSubmit = event => {
-        event.preventDefault();
-    }
+  const [formData, setFormData] = useReducer(formReducer, {});
+  const [submission, setSubmission] = useState({});
 
-    return(
-        <div>
-            <h3>Discussion Thread!</h3>
-            <form onSubmit={handleSubmit}>
-                <div class="container form-group">
-                    <fieldset>
-                        <div class="row form-control-sm">
-                            <input class="col" type="text" name="name" placeholder="Your name" maxLength={40}/>
-                            <div class="col"/>
-                            <input class="col" type="text" name="movie" placeholder="Movie name" maxLength={40}/>
-                        </div>
-                        <div class="row form-control-sm">
-                            <input class="col" type="text" name="title" placeholder="Post title" maxLength={40}/>
-                            <div class="col"/>
-                            <div class="col container">
-                              <div class="row">
-                                <input disabled="true" class="col" value="Rating"/>
-                                <select  class="col">
-                                  <option>5 Star</option>
-                                  <option>4 Star</option>
-                                  <option>3 Star</option>
-                                  <option>2 Star</option>
-                                  <option>1 Star</option>
-                                </select>
-                              </div>
-                            </div>
-                        </div>
-                        <div class="row form-control-sm">
-                            <textarea type="text" name="body" placeholder="Discuss here..." maxLength={500}/>
-                        </div>
-                    
-                    </fieldset>
-                </div>
-                <div class="text-center">
-                    <button class="mt-3" type="submit">Submit</button>
-                </div>
-                
-            </form>
+  const handleSubmit = event => {
+      event.preventDefault();
+      console.log("Submit button was pressed");
+      console.log(formData);
+  }
+
+  const handleChange = event => {
+    setFormData({
+      name: event.target.name,
+      value: event.target.value
+    });
+  }
+
+  return(
+    <div>
+      <h3>Discussion Thread!</h3>
+      <form onSubmit={handleSubmit}>
+        <div class="container form-group">
+          <div class="row form-control-sm">
+            <input class="col" type="text" name="name" onChange={handleChange} placeholder="Your name" maxLength={40}/>
+            <div class="col"/>
+            <input class="col" type="text" name="movie" onChange={handleChange} placeholder="Movie name" maxLength={40}/>
+          </div>
+          <div class="row form-control-sm">
+            <input class="col" type="text" name="title" onChange={handleChange} placeholder="Post title" maxLength={40}/>
+            <div class="col"/>
+            <div class="col container">
+              <div class="row align-items-center">
+                <span class="col h4">Select A Rating</span>
+                <select  class="col" name="rating" onChange={handleChange}>
+                  <option>1 Star</option>
+                  <option>2 Star</option>
+                  <option>3 Star</option>
+                  <option>4 Star</option>
+                  <option>5 Star</option>
+                </select>
+              </div>
+            </div>
+          </div>
+          <div class="row form-control-sm">
+              <textarea type="text" name="body" onChange={handleChange} placeholder="Discuss here..." maxLength={500}/>
+          </div>
+            
         </div>
-    );
+        <div class="text-center">
+            <button class="mt-3" type="submit">Submit</button>
+        </div>
+          
+      </form>
+      <hr/>
+    </div>
+  );
 }
 
 export default Discussion;
