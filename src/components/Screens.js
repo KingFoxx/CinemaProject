97eb8669de28a4c31cfec95ca79c4f83deab9a9d
King.Dev <King.Dev>
2022-05-17 13:23:42 +0100

    Our screens page now looks a tiny bit better

--- src/components/Screens.js
+++ src/components/Screens.js
@@ -10,13 +10,13 @@ const Screens = () => {
 <h1 className="header">Our Screens</h1><br/>
 <div className="newbody App">
 <img src={Screen1} alt="Screen1" className="newimg"/>
-<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo in, impedit quod dicta est facilis consectetur laborum eius vitae voluptate asperiores, vel ipsa laboriosam odit esse beatae nesciunt architecto deserunt.</p>
+<p>Sit in our cheapest seats imaginable! Folding chairs that offer no support for the duration of the movie. Be quick, your seat might be broken or sticky. None of which is our problem.</p>
 
     <img src={Screen2} alt="Screen2"className="newimg" />
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo in, impedit quod dicta est facilis consectetur laborum eius vitae voluptate asperiores, vel ipsa laboriosam odit esse beatae nesciunt architecto deserunt.</p>
+    <p>Sit in our cheapest seats imaginable! Folding chairs that offer no support for the duration of the movie. Be quick, your seat might be broken or sticky. None of which is our problem.</p>
 
         <img src={DirectorBox} alt="Directorbox" className="newimg"/>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo in, impedit quod dicta est facilis consectetur laborum eius vitae voluptate asperiores, vel ipsa laboriosam odit esse beatae nesciunt architecto deserunt.</p>
+        <p> Bringing an innovative lifestyle approach to our venues, where you swap your soft drink for a nice glass of red wine and a slice of freshly made pizza served to your seat. We create a warm and friendly atmosphere, with an excellent food and drink selection and fantastic customer service. With a wide array of mainstream, independent and classic films, special events, launches and a diverse calendar of live satellite broadcasts, there is something for everyone</p>
             </div>
             </div>
 
