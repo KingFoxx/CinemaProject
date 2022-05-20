732de6b04ef344be8f0a358d2a336aca9a0dba79
Mateusz Chocianowski <ch.mateusz@hotmail.com>
2022-05-20 10:00:19 +0100

    complete search fucntionality

--- src/components/filmSearch.js
+++ src/components/filmSearch.js
@@ -1,19 +1,31 @@
-import React,{useRef, useState} from 'react';
-import Film from '../Bookings/film.json';
+import "..//App.css";
+import { Grid } from "@material-ui/core";
+import React, { useRef, useState } from "react";
+import Films from "../Bookings/film.json";
+import Film from "../Bookings/Film";
 
-function Search(){
-    const searchRef = useRef(null);
-
-    const searchName = window.localStorage.getItem('name');
-    console.log("searchname: " + searchName);
-    const item = Film.filter(function(object) {
-        if (object.name === searchName) {
-            searchRef.current = object;
-            console.log("The searched object is: " + object);
-            console.log("And the object item is: " + object.name);
-     }
-   });
-   console.log(item);
-}
-
+const Search = (props) => {
+  const searchRef = useRef(null);
+  const searchName = window.localStorage.getItem("name");
+  console.log("searchname: " + searchName);
+  const item = Films.filter(function (object) {
+    return object.name === searchName;
+  }).map(function (object, i) {
+    searchRef.current = object;
+    console.log("The searched object is: ", object);
+    console.log("And the object item is: " + object.name);
+    return (
+      <Grid key={object.id} item xs={12} lg={4}>
+        <Film film={object} i={i} />
+      </Grid>
+    );
+  });
+  return (
+    <div className="App OpeningTimes"><br/>
+      <Grid container justify="center" spacing={2}>
+        {item}
+      </Grid>
+    </div>
+  );
+};
 export default Search;
